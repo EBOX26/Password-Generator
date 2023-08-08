eeb5c4e5a9eb6b472c8c7ddb7f1bfd3d8ab88668
unknown <ejboland97@gmail.com>
2023-08-07 23:50:53 -0400

    Updated JavaScript code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Password-Generator
 A password generator made using JavaScript
+
+This is a password generator made as a third week challenge.
+In this challenge I was given starter code that had completed HTML and CSS, with the goal of completing the JavaScript file myself. 
+This password generator will (when generate password button is clicked) display a series of prompts for the user to answer.
+First it will ask how many characters the user would like in the passsword being between 8 - 128. If outside of that parameter an alert will be prompted. 
+Then prompts asking if user would like numbers, special characters, uppercase letters, and lowercase letters. 
+If none of these prompts are chosesn then an alert will be prompted and then looped back through the prompts. 
+After all parameters have been chosen then the password generator will make a password displaying it on the page. 
+The JavaScript file has been pseudocoded and commented through my thought process 
+
+The diployed URL of the challenge:
+
--- script.js
+++ script.js
@@ -1,5 +1,107 @@
-// Assignment code here
+// Arrays
+var number = ['1','2','3','4','5','6','7','8','9','0'];
 
+var special = ['~','!','@','#','$','%','^','&','*','_','+',']','[','{','}','>','<','*'];
+
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'];
+
+
+// variables for user feedback
+var userLength = '';
+var userNumber;
+var userSpecial;
+var userUpper;
+var userLower;
+
+function generatePassword() {
+  //ignore button clicked
+  console.log('Button clicked!');
+  // add prompt for password criteria, starting with length
+  var userLength = prompt(
+    'How many characters would you like your password to have? Please choose between 8 and 128.'
+  );
+
+  // while loop will execute if parameters on length are not met, user will be alerted
+  while (userLength <= 7 || userLength >= 129) {
+    alert('Password length must be between 8-128 characters Try again.');
+    var userLength = prompt(
+      'How many characters would you like your password to have?'
+    );
+  }
+  // used alert to repeat users answer
+  alert('Your password will have ' + userLength + ' characters.');
+
+  // collect parameters of password
+  // number, special, upper, lower
+  // used confirm instead of prompt to allow for only yes or no answers
+  var userNumber = confirm(
+    'Click OK if you would like to have numbers, Cancel if not.'
+  );
+  var userSpecial = confirm(
+    'Click OK if you would like to have special characters, Cancel if not.'
+  );
+  var userUpper = confirm(
+    'Click OK if you would like to have uppercase letters, Cancel if not.'
+  );
+  var userLower = confirm(
+    'Click OK if you would like to have lowercase letters, Cancel if not.'
+  );
+ 
+  // all variables === to false will cause while loop to execute, user will be alerted
+  while (
+    userNumber === false &&
+    userSpecial === false &&
+    userUpper === false &&
+    userLower === false
+  ) {
+    alert('You must choose at least one type of character.');
+
+    var userNumber = confirm(
+      'Click OK if you owuld like to have numbers, Cancel if not.'
+    );
+    var userSpecial = confirm(
+      'Click OK if you would like to have special characters, Cancel if not.'
+    );
+    var userUpper = confirm(
+      'Click OK if you would like to have uppercase letters, Cancel if not.'
+    );
+    var userLower = confirm(
+      'Click OK if you would like to have lowercase letters, Cancel if not.'
+    );
+  }
+
+  var passwordChar = [];
+  // used concat method to add certain characters to passwordChar if confirmed by the user 
+
+  if (userNumber) {
+    passwordChar = passwordChar.concat(number);
+  }
+  
+  if (userSpecial) {
+    passwordChar = passwordChar.concat(special);
+  }
+
+  if (userUpper) {
+    passwordChar = passwordChar.concat(upper);
+  }
+
+  if (userLower) {
+    passwordChar = passwordChar.concat(lower);
+  }
+
+
+  var random = '';
+  // for loop
+  for (var i = 0; i < userLength; i++) {
+    random =
+      random +
+      passwordChar[Math.floor(Math.random() * passwordChar.length)];
+    console.log(random);
+  }
+  return random;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +112,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
